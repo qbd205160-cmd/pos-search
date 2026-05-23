8e161b3271dc1705e123d518258f7b5f26e5e2fa
qbd205160-cmd <qbd205160@gmail.com>
2026-05-23 21:38:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-// Force clear all old caches
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(names.map(function(n) { return caches.delete(n); }));
-    })
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch', function(e) {
-  // No caching - always fetch fresh
-});
+self.addEventListener('install',function(e){self.skipWaiting();});self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.map(function(k){return caches.delete(k);}));}));self.clients.claim();});self.addEventListener('fetch',function(e){});
